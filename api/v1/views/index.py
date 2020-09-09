a0200efc698d08ac3f03d5089505a6b580a59120
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-09 22:48:46 +0000

    endopoint stat

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 """Routes status and returns it as JSON"""
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.amenity import Amenity
 
 
 @app_views.route('/status')
 def status():
     """Funtion to get the status of a function
-
-    Returns:
-        [json]: [status]
     """
     return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def stats():
+    """Endpoint that retrieves the number of
+    each objects by type"""
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
