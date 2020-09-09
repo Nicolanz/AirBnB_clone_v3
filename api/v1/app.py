2364fc6e234b3c5d14f05e03f54c7bf140150152
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-09 23:40:51 +0000

    404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module to register the blueprint and the it creates a
+"""Module to register the blueprint and then it creates a
 connection with Flask"""
 from os import environ
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -15,11 +15,13 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 @app.teardown_appcontext
 def teardown(exc):
     """invokes close function of stotage
-
-    Args:
-        exc ([type]): [argument]
     """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error handler"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     host_name = environ['HBNB_API_HOST']
